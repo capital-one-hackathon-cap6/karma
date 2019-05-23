cbf9444e6ec8f873c51c67f202a4f84ef3e02790
Michael Wan <m.wan@berkeley.edu>
2019-05-23 13:05:01 -0400

    Update db json formatting

--- test.py
+++ test.py
@@ -9,12 +9,15 @@ config = {
 
 firebase = pyrebase.initialize_app(config).database()
 
-def push_data(card_id, location, time):
+def push_data(card_id, lat, lon, time):
 	export_data = {
 		'card_id': card_id,
-		'location': location,
+		'location': {
+			'lat': lat,
+			'long': lon
+		},
 		'time': time
 	}
 	firebase.child("server_requests").child(card_id).update(export_data)
 
-push_data('12345678', '(123.4, 1531.3)', 'January 2nd')
+push_data('12345678', 38.9072, 77.0369, 'May 23rd')
